404ab7dc0bbd00a5084aebb00abc81ba47c31162
haiming <1072950174@qq.com>
2019-06-21 16:46:51 +0800

    提交

---
+++ src/routes/Counter/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+class App extends React.Component {
+  log = () => {
+    console.log('不要点我')
+  }
+  render () {
+    return (
+      <React.Fragment>
+        我是测试用的counter
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
