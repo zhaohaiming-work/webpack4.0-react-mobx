43909eb41a84effb379ca5234686fe549b31f8a2
haiming <1072950174@qq.com>
2019-07-15 14:41:19 +0800

    增加了func模块

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -56,6 +56,7 @@ const config = {
       store: resolve(`${srcDir}/mobx/index`),
       imgage: resolve(`${srcDir}/images`),
       api: resolve(`${srcDir}/api`),
+      func: resolve(`${srcDir}/func`)
     }
   },
   externals,
