8f754f6e3f508a86cf6e90f6632894af265544ec
haiming <1072950174@qq.com>
2019-09-12 17:27:58 +0800

    添加了过度页面的进度条显示

--- src/components/loadable.js
+++ src/components/loadable.js
@@ -1,6 +1,15 @@
 import React from 'react'
-// import propTypes from 'prop-types'
-import './common'
-const OverLoadTmpl = <div className='loading-process' />
-
-export default OverLoadTmpl
+import nprogress from 'nprogress'
+class OverLoadTmpl extends React.Component {
+  constructor (props) {
+    super(props)
+    nprogress.start()
+  }
+  componentWillMount () {
+    nprogress.done()
+  }
+  render () {
+    return null
+  }
+}
+export default <OverLoadTmpl />
