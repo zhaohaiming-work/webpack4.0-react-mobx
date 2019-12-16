3517c42c9fddd1d5f4e4e887d6648808b9fb56ac
zhaohaiming-work <1072950174@qq.com>
2019-12-16 10:39:57 +0800

    Update webpack.config.js
    去掉缓存

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -118,7 +118,7 @@ const babelLoader = {
 config.plugins.push(
   new HappyPack({
     id: 'happyBabel',
-    cache: false,
+    //cache: false,
     loaders: [babelLoader],
     //共享进程池
     threadPool: happyThreadPool,
