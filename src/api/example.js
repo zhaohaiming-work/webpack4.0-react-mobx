2cb5745203bf34c136bd6b63d57cdf8113745660
haiming <1072950174@qq.com>
2020-04-16 17:04:47 +0800

    清除项目

--- src/api/example.js
+++
@@ -1,8 +0,0 @@
-import { ajax } from './'
-class Xhr {
-  getChainDate = (postData = {}) => (
-    ajax.get('/asset2/api/project/chainDateList', { params: postData })
-  )
-}
-
-export default new Xhr()
